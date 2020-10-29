6dfda65fa4634add44a692645054d179435ac014
Justin Lee <leej1996@gmail.com>
2020-10-29 12:05:42 -0400

    Clean up comments

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ annotations = np.zeros((5,9))
 i = 0
 with open("annotations.txt", "r") as a_file:
     for line in a_file:
-        #strip each line, remove name of image and then create an np array to store information
+        # strip each line, remove name of image and then create an numpy array to store information
         stripped_line = line.strip()
         stripped_list = np.array(stripped_line.lstrip(img_list[i]).split())
-        #Append zeros to end if size is too small (need size 9 array)
+        # Append zeros to end if size is too small (need size 9 array for each row)
         if stripped_list.size == 5:
             stripped_list = np.append(stripped_list,[0,0,0,0])
         annotations[i] = stripped_list
@@ -20,12 +20,12 @@ with open("annotations.txt", "r") as a_file:
 
 annotations = annotations.astype(np.int32)
 
-#Iterate through all images, and draw rectangeles based off of annotations.txt
+# Iterate through all images, and draw rectangles based off of annotations.txt
 i = 0
 for name in img_list:
     img = cv.imread("images/" + name)
-    #Check if we draw one or two rectangles, then draw them
-    if (annotations[i][0] == 1):
+    # Check if we draw one or two rectangles, then draw them in green and red (for second one)
+    if annotations[i][0] == 1:
         cv.rectangle(img,(annotations[i][1],annotations[i][2]),(annotations[i][3],annotations[i][4]),(0,255,0),3)
     else:
         cv.rectangle(img,(annotations[i][1],annotations[i][2]),(annotations[i][3],annotations[i][4]),(0,255,0),3)
